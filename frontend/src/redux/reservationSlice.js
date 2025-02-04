8e5e6192be42766569d6e8e0f673e51477dfc8b0
Ashikusman <106094677+Ashikusman@users.noreply.github.com>
2025-02-04 11:49:05 +0530

    Add files via upload

--- frontend/src/redux/reservationSlice.js
+++ frontend/src/redux/reservationSlice.js
@@ -9,15 +9,15 @@ const initialState = {
     // startTime: "",
     // year: "",
    reservationList: []
+   //reservationList: JSON.parse(localStorage.getItem('reservationLists')) || [], // Load from localStorage if available
+   
 };
 
 export const reservationSlice = createSlice({
     name : "reservation",
     initialState,
     reducers : {
-        setDatareservation : (state, action) => {
-            // console.log(action.payload.data)
-
+        setDatareservation : (state, action) => {    
             // state.noOfPerson = action.payload.data.noOfPerson
             // state.date = action.payload.data.date
             // state.month = action.payload.data.month
@@ -26,6 +26,8 @@ export const reservationSlice = createSlice({
             // state.endTime = action.payload.data.endTime
             // state.selectTable = action.payload.data.selectTable
             state.reservationList = [...action.payload];
+            //localStorage.setItem('reservationLists', JSON.stringify(state.reservationList)); // Persist to localStorage
+            //console.log(action);
         }
     }
 
